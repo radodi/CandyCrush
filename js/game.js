79205cd038aa45d23447efc08a9287ad46b0daae
Radoslav <r1921d@gmail.com>
2017-07-07 01:30:11 +0300

    :bug: fixes

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
 var gameboard = [["R","G","G","B"], 
 				 ["B","O","G","G"], 
-				 ["P","O","B","P"], 
+				 ["P","Y","B","P"], 
 				 ["Y","B","Y","O"]],
-cell = [0, 1],
+cell,
 newCell,
-direction = 'D';
+direction;
 var xLen = gameboard[0].length;
 var yLen = gameboard.length;
 // checkDirection - Checks for a valid move direction
@@ -17,7 +17,7 @@ function checkDirection () {
 			}
 			break;
 		case 'R':
-			if (cell[1] < gameboard[cell[0]].length) {
+			if (cell[1] < gameboard[cell[0]].length-1) {
 				newCell=[cell[0],(cell[1]+1)];
 				return true;
 			}
@@ -39,35 +39,40 @@ function checkDirection () {
 //doMove() - Changes the positions (values) of two elements in the specified direction
 // and set new cell value
 function doMove(){
-	var temp;
+	var temp,v1,v2;
+	console.log('-------cell-------');
 	console.log(cell);
-	console.log('--------------');
-	console.log(gameboard);
+	console.log('------------------');
+	// console.log(gameboard);
 	switch(direction){
 		case 'L':
-			temp = gameboard[cell[0]][(cell[1]-1)];
-			gameboard[cell[0]][(cell[1]-1)] = gameboard[cell[0]][cell[1]];
-			gameboard[cell[0]][cell[1]] = temp;
+			v1 = gameboard[cell[0]][cell[1]];
+			v2 = gameboard[cell[0]][cell[1]-1];
+			gameboard[cell[0]][cell[1]] = v2;
+			gameboard[cell[0]][cell[1]-1] = v1;
 			break;
 		case 'R':
-			temp = gameboard[cell[0]][(cell[1]+1)];
-			gameboard[cell[0]][(cell[1]+1)] = gameboard[cell[0]][cell[1]];
-			gameboard[cell[0]][cell[1]] = temp;
+			v1 = gameboard[cell[0]][cell[1]];
+			v2 = gameboard[cell[0]][cell[1]+1];
+			gameboard[cell[0]][cell[1]] = v2;
+			gameboard[cell[0]][cell[1]+1] = v1;
 			break;
 		case 'U':
-			temp = gameboard[(cell[0]-1)][cell[1]];
-			gameboard[(cell[0]-1)][cell[1]] = gameboard[cell[0]][cell[1]];
-			gameboard[cell[0]][cell[1]] = temp;
+			v1 = gameboard[cell[0]][cell[1]];
+			v2 = gameboard[cell[0]-1][cell[1]];
+			gameboard[cell[0]][cell[1]] = v2;
+			gameboard[cell[0]-1][cell[1]] = v1;
 			break;
 		case 'D':
-			temp = gameboard[(cell[0]+1)][cell[1]];
-			gameboard[(cell[0]+1)][cell[1]] = gameboard[cell[0]][cell[1]];
-			gameboard[cell[0]][cell[1]] = temp;
+			v1 = gameboard[cell[0]][cell[1]];
+			v2 = gameboard[cell[0]+1][cell[1]];
+			gameboard[cell[0]][cell[1]] = v2;
+			gameboard[cell[0]+1][cell[1]] = v1;
 			break;
 	}
-	console.log('--------------');
-	console.log(gameboard);
-	return gameboard ;
+	// console.log('--------------');
+	// console.log(gameboard);
+	 return true;
 }
 // checkUp - Checks for matching up until reach a different value
 // and returns a number of matches
@@ -75,9 +80,9 @@ function checkUp(arg){
 	if (arg[0] < 1) {
 		return 0;
 	} else {
-		var num = 0, n=1;
+		var num = 0, n=1, m;
 		for (var i = arg[0]; i > 0; i--) {
-			if (gameboard[arg[0]][arg[1]] === gameboard[(arg[0]-n)][arg[1]]) {
+			if (gameboard[arg[0]][arg[1]] == gameboard[arg[0]-n][arg[1]]) {
 				num++;
 				n++;
 			} else {
@@ -89,14 +94,15 @@ function checkUp(arg){
 }
 // checkDown - Checks for matching down until reach a different value
 // and returns a number of matches
-function checkDown(arg){
+function checkDown(arg, yLen){
 	if (arg[0] === yLen-1) {
 		return 0;
 	} else {
 		var num = 0, n=1;
 		for (var i = arg[0]; i < yLen-1; i++) {
-			if (gameboard[arg[0]][arg[1]] === gameboard[(arg[0]+n)][arg[1]]) {
+			if (gameboard[arg[0]][arg[1]] == gameboard[arg[0]+n][arg[1]]) {
 				num++;
+			console.log('n'+n);
 				n++;
 			} else {
 				return num;
@@ -113,7 +119,7 @@ function checkLeft(arg){
 	} else {
 		var num = 0, n=1;
 		for (var i = arg[1]; i > 0; i--) {
-			if (gameboard[arg[0]][arg[1]] === gameboard[arg[0]][(arg[1]-n)]) {
+			if (gameboard[arg[0]][arg[1]] == gameboard[arg[0]][arg[1]-n]) {
 				num++;
 				n++;
 			} else {
@@ -125,13 +131,13 @@ function checkLeft(arg){
 }
 // checkRight - Checks for matching right until reach a different value
 // and returns a number of matches
-function checkRight(arg){
+function checkRight(arg, xLen){
 	if (arg[1] === xLen-1) {
 		return 0;
 	} else {
 		var num = 0, n=1;
 		for (var i = arg[1]; i < xLen; i++) {
-			if (gameboard[arg[0]][arg[1]] === gameboard[arg[0]][(arg[1]+n)]) {
+			if (gameboard[arg[0]][arg[1]] == gameboard[arg[0]][arg[1]+n]) {
 				num++;
 				n++;
 			} else {
@@ -144,13 +150,17 @@ function checkRight(arg){
 // matches - The function checks for more than 2 horizontal and vertical matching values
 // and puts the coordinates of the values in a new array.
 function matches(gameboard, startCell, valuesArr){
+	console.log('-------valuesArr--------');
 	console.log(valuesArr);
+	console.log('-------startCell--------');
+	console.log(startCell);
+
 	var matchesArr=[startCell];
 	if (valuesArr[0]+valuesArr[1] > 1) {
 		for (var i = valuesArr[0]; i > 0; i--) {
 			matchesArr.push([startCell[0],(startCell[1]-i)]);
 		}
-		for (var j = valuesArr[0]; j > 0; j--) {
+		for (var j = valuesArr[1]; j > 0; j--) {
 			matchesArr.push([startCell[0],(startCell[1]+j)]);
 		}
 	}
@@ -162,28 +172,26 @@ function matches(gameboard, startCell, valuesArr){
 			matchesArr.push([(startCell[0]+l), startCell[1]]);
 		}
 	}
+	console.log('-------matchesArr--------');
+	// console.log(matchesArr);
 	return matchesArr;
 }
 
 function candyCrush(gameboard, cell, direction) {
 	if (checkDirection()) {
-		doMove();
-		var matchesArr = matches(gameboard, newCell,[checkUp(newCell),checkDown(newCell), checkLeft(newCell), checkRight(newCell)]);
-		console.log(matchesArr);
-		if (matchesArr.length <3) {
-			matchesArr = matches(gameboard, cell,[checkUp(cell),checkDown(cell), checkLeft(cell), checkRight(cell)]);
-			if (matchesArr.length > 2) {
-				console.log(true);
-			}
-		} else {
-			console.log(true);
-		}
+		doMove(gameboard, cell, direction);
+		var mArr = matches(gameboard, newCell,[checkLeft(newCell),checkRight(newCell,xLen),checkUp(newCell),checkDown(newCell, yLen)]);
+		console.log(mArr);
+		mArr = matches(gameboard, cell,[checkLeft(cell),checkRight(cell,xLen),checkUp(cell),checkDown(cell, yLen)]);
+		console.log(mArr);
+		generateBoard();
 	}
 }
 // candyCrush(gameboard, cell, direction);
 // generateBoard - Gnerate GameBoard in HTML Doccument
 function generateBoard(){
 	$('#container').css('width', xLen*100+20+'px');
+	$('.row').remove();
 	for (var i = 0; i < yLen; i++) {
 		$('#container').append($('<div class="row" id="row_'+i+'">'));
 		for (var j = 0; j < xLen; j++) {
@@ -199,26 +207,27 @@ $(document).ready(function(){
 	});
 	$(document).keydown(function(e){
 		var selected = $('.selected');
+		switch (e.keyCode) {
+			case 37:
+			direction = 'L';
+			break;
+			case 39:
+			direction = 'R';
+			break;
+			case 38:
+			direction = 'U';
+			break;
+			case 40:
+			direction = 'D';
+			break;
+		}
 		if(selected.length === 0){
-			alert("Select Cocktail to move!");
+			// alert("Select Cocktail to move!");
 		} else {
 			var y = $('.selected').parent().attr('id').split('row_').slice(-1);
 			var x = $('.selected').attr('id').split('col_').slice(-1);
-			cell = [y[0],x[0]];
+			cell = [parseInt(y[0]),parseInt(x[0])];
+			candyCrush(gameboard, cell, direction);
 		}
-		switch (e.keyCode) {
-		case 37:
-         	direction = 'L';
-         	break;
-		case 39:
-         	direction = 'R';
-			break;
-		case 38:
-         	direction = 'U';
-         	break;
-		case 40:
-			direction = 'D';
-			break;
-    }
 	});
 });
